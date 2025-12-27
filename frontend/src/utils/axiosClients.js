dcc21ca8468145ec56ec73dafd897218fa7e7002
shubham-kumar145 <shubhamkr5923@gmail.com>
2025-12-27 19:21:12 +0530

    Update axiosClients.js

--- frontend/src/utils/axiosClients.js
+++ frontend/src/utils/axiosClients.js
@@ -1,15 +1,24 @@
-import axios from "axios"
+// import axios from "axios"
 
-const axiosClient =  axios.create({
-    baseURL: 'https://stylish-wear-aesthetics-swa.onrender.com',
+// const axiosClient =  axios.create({
+//     baseURL: 'https://stylish-wear-aesthetics-swa.onrender.com',
+//     withCredentials: true,
+//     headers: {
+//         'Content-Type': 'application/json'
+//     }
+// });
+
+
+// export default axiosClient;
+
+import axios from "axios";
+
+const axiosClient = axios.create({
+    baseURL: "https://stylish-wear-aesthetics-swa.onrender.com",
     withCredentials: true,
     headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
     }
 });
 
-
 export default axiosClient;
-
-
-
