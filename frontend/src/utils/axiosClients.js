04cfdfe12a32b0d37071da720207ccfaf29b713a
shubham-kumar145 <shubhamkr5923@gmail.com>
2025-12-30 17:53:46 +0530

    Update axiosClients.js

--- frontend/src/utils/axiosClients.js
+++ frontend/src/utils/axiosClients.js
@@ -1,24 +1,11 @@
-// import axios from "axios"
-
-// const axiosClient =  axios.create({
-//     baseURL: 'https://stylish-wear-aesthetics-swa.onrender.com',
-//     withCredentials: true,
-//     headers: {
-//         'Content-Type': 'application/json'
-//     }
-// });
-
-
-// export default axiosClient;
-
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "https://stylish-wear-aesthetics-swa.onrender.com",
-    withCredentials: true,
-    headers: {
-        "Content-Type": "application/json"
-    }
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default axiosClient;
